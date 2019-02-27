6b22d62a134d3f99458a13f045c5b8eb08bd08eb
Alchemista-K <alchemista.k582@gmail.com>
2019-02-27 14:33:33 +0000

    バリデーションとして文字数制限を追加

--- app/models/shop_database.rb
+++ app/models/shop_database.rb
@@ -1,11 +1,22 @@
 class ShopDatabase < ApplicationRecord
   
-  # 空の投稿を制限するバリデーション(shop_name、introduction、address、tel_number、comment)
+  
+=begin
+  空の投稿を制限するバリデーション
+  (shop_name、introduction、address、tel_number、comment)(2019.2.26)
+=end
+=begin  
+  文字数を制限するバリデーション
+  introductionは最大200文字、addressは最大50文字 
+  tel_numberは最大11文字、commentは最大200文字
+  (2019.2.27)
+=end
+
   validates:shop_name, {presence: true}
-  validates:introduction, {presence: true}  
-  validates:address, {presence: true} 
-  validates:tel_number, {presence: true}   
-  validates:comment, {presence: true}  
+  validates:introduction, {presence: true, length:{maximum:200}}  
+  validates:address, {presence: true, length:{maximum:50}}
+  validates:tel_number, {presence: true, length:{maximum:11}}
+  validates:comment, {presence: true, length:{maximum:200}}  
   
   
         #写真アップロード
