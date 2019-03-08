d757501c75a23c58a471cc5234aaf149bb6f1a2a
Alchemista-K <alchemista.k582@gmail.com>
2019-03-08 12:18:23 +0000

    プロフィール画像をアップロードできるようにカラムを追加したつもり

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_152827) do
+ActiveRecord::Schema.define(version: 2019_03_08_121019) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
